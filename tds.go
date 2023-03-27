fc317f81d698c7fe3ebeca11e203043b2168a403
Stephanie Hingtgen <hingtgen.stephanie@gmail.com>
2023-03-27 12:41:57 -0600

    Update tds.go

--- tds.go
+++ tds.go
@@ -845,18 +845,18 @@ type auth interface {
 // list of IP addresses.  So if there is more than one, try them all and
 // use the first one that allows a connection.
 func dialConnection(ctx context.Context, c *Connector, p connectParams) (conn net.Conn, err error) {
+	// if the dialer has been updated, the dialer may be proxying to a different network, and so the
+	// dialer should be used to connect so the DNS is resolved within the right network
+	if c.Dialer != nil {
+		d := c.getDialer(&p)
+		addr := net.JoinHostPort(p.host, strconv.Itoa(int(resolveServerPort(p.port))))
+		return d.DialContext(ctx, "tcp", addr)
+	}
 	var ips []net.IP
 	ip := net.ParseIP(p.host)
 	if ip == nil {
 		ips, err = net.LookupIP(p.host)
 		if err != nil {
-			// if the dialer has been updated and DNS can not be resolved, the dialer may be trying to
-			// proxy to a different network where the domain name is local. Try to connect via the custom dialer
-			if c.Dialer != nil {
-				d := c.getDialer(&p)
-				addr := net.JoinHostPort(p.host, strconv.Itoa(int(resolveServerPort(p.port))))
-				return d.DialContext(ctx, "tcp", addr)
-			}
 			return
 		}
 	} else {
